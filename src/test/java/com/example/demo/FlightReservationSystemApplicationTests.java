7377a23c384eb7452cee3b40d758e25aa62debef
Ramaswamy24 <90293313+Ramaswamy24@users.noreply.github.com>
2022-05-04 20:54:13 +0530

    Update FlightReservationSystemApplicationTests.java

--- src/test/java/com/example/demo/FlightReservationSystemApplicationTests.java
+++ src/test/java/com/example/demo/FlightReservationSystemApplicationTests.java
@@ -2,8 +2,8 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import com.flight.FlightReservationSystemApplication;
+@SpringBootTest(classes = FlightReservationSystemApplication.class)
 class FlightReservationSystemApplicationTests {
 
 	@Test
